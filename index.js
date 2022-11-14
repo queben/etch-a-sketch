170436e71ec21f7b15aa7298e1c5552f92b663c7
Kevin <queben.quin@gmail.com>
2022-11-13 19:09:50 -0600

    fix duplicated top row

--- index.js
+++ index.js
@@ -5,18 +5,22 @@ const row = 16;
 const column = 16;
 
 for(let x = 0; x < row; x++) {
-    let y = 0;
-
     let divX = document.createElement('div')
 
-    divX.setAttribute("id", `x${x}y${y}`)
+    divX.setAttribute("id", `x${x}yN`)
     divX.textContet = 'hi'
     boxContainer.appendChild(divX)
 
-    for(y = 0; y < column; y++) {
+    for(let y = 0; y < column; y++) {
         let divY = document.createElement('div')
 
         divY.setAttribute('id', `x${x}y${y}`)
         divX.appendChild(divY)
     }
+}
+
+for(let x = 0; x < row; x++) {
+    const divvyRemove = document.querySelector(`#x${x}yN`)
+    divvyRemove.style.borderColor = 'blue';
+    divvyRemove.style.border = '0px';
 }
