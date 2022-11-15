902faf944af968aaa12176aba93b1af6e8c14b41
Kevin <queben.quin@gmail.com>
2022-11-15 00:21:11 -0600

    refactor and remove unnecessary code. add styling to elements. project is complete!

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 const boxContainer = document.querySelector(".boxContainer")
 const slider = document.querySelector("#slide-value")
-const sliderValue = document.querySelector(".grid-size")
+const sliderValue = document.createElement("li")
+const uList = document.querySelector('ul')
+
+sliderValue.innerText = `Grid Size: ${slider.value} x ${slider.value}`;
+sliderValue.classList.add('grid-size')
+uList.prepend(sliderValue)
 
 let row = slider.value;
 let column = slider.value;
@@ -9,8 +14,7 @@ let isColoring = false;
 createGrid()
 
 slider.oninput = () => {
-    sliderValue.innerText = `Grid Size: ${slider.value}`;
-    console.log(sliderValue.innerHTML)
+    sliderValue.innerText = `Grid Size: ${slider.value} x ${slider.value}`;
     row = slider.value;
     column = slider.value;
     deleteGrid();
@@ -22,16 +26,11 @@ function createGrid() {
         let divX = document.createElement('div')
     
         divX.setAttribute("id", `x${x}yN`)
-        divX.textContet = 'hi'
-        divX.style.cssText = 'display: flex; flex: 1; flex-flow; row wrap user-select: none;'
-        //divX.style.cssText = "border: solid; background-color: white; border-color: black; margin: 0px; padding: 0px; width: 50px; height: 50px;"
-        //removed line above bc box comes out better without it. boxes looked weird
         boxContainer.appendChild(divX)
         for(let y = 0; y < column; y++) {
             let divY = document.createElement('div')
     
             divY.setAttribute('id', `x${x}y${y}`)
-            divY.style.cssText = "border: solid; border-color: black; border-width: 1px; margin: 0px; padding: 0px; flex: 1; user-select: none;"
             divY.addEventListener('mousedown', () => {
                 isColoring = true;
                 let num1 = Math.floor(Math.random() * 255)
@@ -56,13 +55,7 @@ function createGrid() {
             divX.appendChild(divY)
         }
     }
-    
-    for(let x = 0; x < row; x++) {
-        const divvyRemove = document.querySelector(`#x${x}yN`)
-        divvyRemove.style.borderColor = 'blue';
-        divvyRemove.style.border = '0px';
 
-    }
 }
 
 function deleteGrid() {
